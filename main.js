7e4ed26166aa4643cbe0e3b218b175e24a21dec5
Dean Cook <deancook371@gmail.com>
2021-04-23 12:20:02 -0600

    Add switch view functionality to game difficulty cards

--- main.js
+++ main.js
@@ -4,9 +4,31 @@ var currentGame = new Game();
 
 // Query Selectors
 var changeGameBtn = document.querySelector("#changeGameBtn");
-var regularGameCard = document.querySelector("#regularGameCard");
+var chooseGameDifficulty = document.querySelector("#chooseGameDifficulty");
 var difficultGameCard = document.querySelector("#difficultGameCard");
+var difficultPlayerSelect  = document.querySelector("#difficultPlayerSelect");
+var regularGameCard = document.querySelector("#regularGameCard");
+var regularPlayerSelect = document.querySelector("#regularPlayerSelect");
 
 // Event Listeners
 
+regularGameCard.addEventListener("click", function() {
+  switchView(regularPlayerSelect);
+});
+
+difficultGameCard.addEventListener("click", function() {
+  switchView(difficultPlayerSelect);
+})
+
+changeGameBtn.addEventListener("click", resetGame);
+
 // Functions
+
+function switchView(view) {
+  chooseGameDifficulty.classList.add("hidden");
+  view.classList.remove("hidden");
+}
+
+function resetGame() {
+
+}
