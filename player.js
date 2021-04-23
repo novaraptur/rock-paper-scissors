0c860e19d5cbe91907fb620957deceb9a92974b3
Dean Cook <deancook371@gmail.com>
2021-04-23 08:49:17 -0600

    Add structure to player class

--- player.js
+++ player.js
@@ -1,1 +1,27 @@
 
+class Player {
+  constructor(name, token) {
+    this.name = name;
+    this.token = token;
+    this.wins = 0;
+    this.choice = "";
+  }
+
+  takeTurn(playerChoice) {
+    this.choice = playerChoice;
+  }
+
+  winGame() {
+    this.wins++;
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+}
+
+module.exports = Player;
